0cb5b4309868bfda7f36053cd771389ca0d2ca70
Reuben <reubenhutton@hotmail.com>
2020-02-08 18:23:18 +0000

    Add flatten onject logic

--- source/javascript/src/index.js
+++ source/javascript/src/index.js
@@ -1,4 +1,55 @@
 /**
+ * Flatten Object.
+ * @param source
+ * json object containing configuration that will be flattened
+ * @param targetEnvironment
+ * environment who configs will override default config
+ * @param environmentList
+ * list of all the environments contained in the config
+ * @returns {object}
+ * Flattened json object
+ */
+function flattenObject(
+  sourceObj,
+  targetEnvironment,
+  environmentList) {
+
+    for (const key in sourceObj) {
+
+      var target = sourceObj[key];
+
+      if (isObject(target)) {
+
+        console.log(`Key ${key} is object, recursing`);
+
+        var flattendObj = flattenObject(target, targetEnvironment, environmentList);
+
+      } else {
+
+        console.log(`Key ${key} = ${targetEnvironment}`);
+
+        var flattendObj = target;
+      
+      }
+
+      if(key == targetEnvironment){
+
+        console.log(`Merging and removing Env key ${key}`);
+
+        const envProp = sourceObj[key];
+
+        delete sourceObj[key];
+
+        sourceObj = mergeDeep(sourceObj, envProp)
+      }
+
+    }
+
+    return sourceObj
+}
+
+
+/**
  * Simple object check.
  * @param item
  * @returns {boolean}
@@ -6,6 +57,7 @@
 function isObject(item) {
   return (item && typeof item === 'object' && !Array.isArray(item));
 }
+
 
 /**
  * Deep merge two objects.
@@ -32,8 +84,6 @@ function mergeDeep(target, ...sources) {
 }
 
 module.exports = {
-  mergeDeep: mergeDeep
+  mergeDeep: mergeDeep,
+  flattenObject: flattenObject
 }
-
-const merged = mergeDeep({a: 1}, { b : { c: { d: { e: { f: 12345}}}}});  
-console.log(merged.b.c);
--- source/javascript/test/test.js
+++ source/javascript/test/test.js
@@ -1,23 +1,129 @@
 var assert = require('assert');
 var test = require('../src/index');
 
+/************************* Merge Deep *******************************/
 describe('Merge Deep', function() {
   it('Test Deep Copy', function() {
-    const merged = test.mergeDeep({a: 1}, { b : { c: { d: { e: { f: 12345}}}}})
+
+    var sourceObj = { a: 1 };
+    var targetObj = { b : { c: { d: { e: { f: 12345}}}}};
+
+    const merged = test.mergeDeep(sourceObj, targetObj)
     console.log(merged.b.c);
     assert.equal(merged.b.c.d.e.f, 12345);
   });
 
   it('Test Overwrite', function() {
-    const merged = test.mergeDeep({a: 1}, { a : 2})
+
+    var sourceObj = { a: 1 };
+    var targetObj = { a : 2};
+
+    const merged = test.mergeDeep(sourceObj, targetObj)
     assert.equal(merged.a, 2);
   });
 
   it('Test Array Overwrite', function() {
 
     var arrayRes = [1, 2, 3, 5];
+    var sourceObj = {a: [1, 1, 1, 1]};
+    var targetObj = { a : [1, 2, 3, 5]};
 
-    const merged = test.mergeDeep({a: [1, 1, 1, 1]}, { a : [1, 2, 3, 5]})
+    const merged = test.mergeDeep(sourceObj, targetObj);
     assert.equal(JSON.stringify(merged.a), JSON.stringify(arrayRes));
+  });
+});
+
+
+/************************* Flatten Object *******************************/
+describe('Flatten Object', function() {
+  it('Test Object with no environments', function() {
+
+    const sourceObj = {
+      connectionString: "TestDB1"
+    }
+
+    const targetEnvironment = "TEST"
+    const environmentList = ["TEST", "PROD"]
+
+    const outputObj = test.flattenObject(sourceObj, targetEnvironment, environmentList)
+    assert.equal(sourceObj, outputObj);
+    assert.equal(sourceObj["connectionString"], "TestDB1")
+  });
+
+  it('Test Basic Environment override', function() {
+
+    const sourceObj = {
+      TEST: {
+        connectionString: "TestDB1"
+      },
+      connectionString: "ProdDB1"
+    }
+
+    const targetEnvironment = "TEST"
+    const environmentList = ["TEST", "PROD"]
+
+    const outputObj = test.flattenObject(sourceObj, targetEnvironment, environmentList)
+    assert.equal(sourceObj, outputObj);
+    assert.equal(sourceObj["connectionString"], "TestDB1")
+  });
+
+  it('Test Double Environment override', function() {
+
+    const sourceObj = {
+      TEST: {
+        TEST: {
+          connectionString: "TestDB1"
+        }
+      },
+      connectionString: "ProdDB1"
+    }
+
+    const targetEnvironment = "TEST"
+    const environmentList = ["TEST", "PROD"]
+
+    const outputObj = test.flattenObject(sourceObj, targetEnvironment, environmentList)
+    assert.equal(sourceObj, outputObj);
+    assert.equal(sourceObj["connectionString"], "TestDB1")
+  });
+
+  it('Test Multiple Environment override', function() {
+
+    const sourceObj = {
+      TEST: {
+        connectionString: "TestDB1"
+      },
+      PROD2: {
+        connectionString: "ProdDB2"
+      },
+      connectionString: "ProdDB1"
+    }
+
+    const targetEnvironment = "TEST"
+    const environmentList = ["TEST", "PROD"]
+
+    const outputObj = test.flattenObject(sourceObj, targetEnvironment, environmentList)
+    assert.equal(sourceObj, outputObj);
+    assert.equal(sourceObj["connectionString"], "TestDB1")
+  });
+
+  it('Test array override', function() {
+
+    const sourceObj = {
+      DR: {
+        machineList: ["app1", "app2", "app3", "app4"]
+      },
+      PROD: {
+        machineList: ["pr-app1", "pr-app2", "pr-app3", "pr-app4"]
+      },
+      machineList: ["app1", "app2", "app3", "app4"]
+    }
+
+    var arrayRes = ["pr-app1", "pr-app2", "pr-app3", "pr-app4"];
+
+    const targetEnvironment = "PROD"
+    const environmentList = ["DR", "PROD"]
+
+    const outputObj = test.flattenObject(sourceObj, targetEnvironment, environmentList)
+    assert.equal(JSON.stringify(outputObj.machineList), JSON.stringify(arrayRes));
   });
 });
